5f434f7fd29bc366e4fb1053f7f4aa5cc2498c4a
ian <ianmwendamicheni@gmail.com>
2023-10-26 17:29:00 +0300

    complete

--- src/__tests__/Task.test.js
+++ src/__tests__/Task.test.js
@@ -1,22 +1,20 @@
 import "@testing-library/jest-dom";
 import { render, screen, fireEvent } from "@testing-library/react";
+import React from "react";
 import App from "../components/App";
 import Task from "../components/Task";
 
-test("displays the task text", () => {
-  render(<Task text={"text!"} category={"category!"} />);
+test("displays the task text and category", () => {
+  const task = { text: "text!", category: "category!" };
+  render(<Task task={task} onDeleteTask={() => {}} />);
   expect(screen.queryByText("text!")).toBeInTheDocument();
-});
-
-test("displays the task category", () => {
-  render(<Task text={"text!"} category={"category!"} />);
   expect(screen.queryByText("category!")).toBeInTheDocument();
 });
 
 test("is removed from the list when the delete button is clicked", () => {
   render(<App />);
-  const task = screen.queryByText(/Buy rice/);
-  const deleteButton = task.parentElement.querySelector("button");
+  const taskText = screen.getByText("Buy rice");
+  const deleteButton = taskText.parentElement.querySelector("button");
 
   fireEvent.click(deleteButton);
 
